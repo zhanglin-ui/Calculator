19ff5ebba37a339b3240c4652434edc09acb0621
zl <14757624612@qq.com>
2019-12-01 00:03:25 +0800

    帮助界面和一些bug

---
+++ app/src/main/java/com/example/hello/bangzhu.java
@@ -0,0 +1,13 @@
+package com.example.hello;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class bangzhu extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shuoming);
+    }
+}
